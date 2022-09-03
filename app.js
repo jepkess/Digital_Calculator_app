f1e30c4498438f7a57b1a8290938985537ae1b3e
jepkess <biivincent1997@gmail.com>
2022-09-03 14:13:00 +0300

    add javascript code

---
+++ app.js
@@ -0,0 +1,17 @@
+let currentInput = document.querySelector('.currentInput');
+let answerScreen = document.querySelector('.answerScreen');
+let buttons = document.querySelectorAll('button');
+let erasebtn = document.querySelector('#erase');
+let clearbtn = document.querySelector('#clear');
+let evaluate = document.querySelector('#evaluate');
+let realTimeScreenValue = []
+
+clearbtn.addEventListener("click" , () => {
+
+    realTimeScreenValue = [''];
+    answerScreen.innerHTML = 0;
+    currentInput.className = 'currentInput'
+    answerScreen.className = 'answerScreen';
+    answerScreen.getElementsByClassName.color = "rgba(150, 150, 150, 0.87)";
+})
+
