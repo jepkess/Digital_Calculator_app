250bf155e6a869ff30151c0ec83c59b35b8baa40
jepkess <biivincent1997@gmail.com>
2022-09-03 14:37:20 +0300

    add javascript code

--- app.js
+++ app.js
@@ -14,4 +14,14 @@ clearbtn.addEventListener("click" , () => {
     answerScreen.className = 'answerScreen';
     answerScreen.getElementsByClassName.color = "rgba(150, 150, 150, 0.87)";
 })
+buttons.forEach((btn) =>{
+    btn.addEventListener('click', () =>{
+        // when clicked button is not erased button
+        if (!btn.id.match('erase')){
+            //To display value on btn press
+            realTimeScreenValue.push(btn.value)
+            currentInput.innerHTML = realTimeScreenValue.join('');
+        }
+    })
+})
 
